7b2a721824b27d2f1736a1a9fdd8a6ba49e77d6b
Harsh Shah <harshdev624@gmail.com>
2024-04-19 13:58:33 +0530

    Version 1.1

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "@firebase/firestore"
+import { getFirestore } from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjgNhUNznNBOfG87Nxwk2k78nvroKcY58",
-  authDomain: "shareme-654a9.firebaseapp.com",
-  projectId: "shareme-654a9",
-  storageBucket: "shareme-654a9.appspot.com",
-  messagingSenderId: "560295369241",
-  appId: "1:560295369241:web:bb0f774367b59e8878a888"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app)
+export const db = getFirestore(app);
