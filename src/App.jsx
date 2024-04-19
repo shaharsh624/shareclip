383d8cbfa0892d2d75119809ff266b0089d784da
Harsh Shah <harshdev624@gmail.com>
2024-04-19 14:58:09 +0530

    Added Input in Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import {
-    Card,
+    Button,
     Heading,
     Image,
     InputGroup,
     InputLeftAddon,
-    InputRightAddon,
-    Spacer,
-    Stack,
     Text,
 } from "@chakra-ui/react";
 import "./App.css";
-import Page from "./Page";
 import { Input } from "@chakra-ui/react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
     const [site, setSite] = useState("");
+    const navigate = useNavigate();
+
+    const handleGoto = (e) => {
+        e.preventDefault();
+        navigate(`/${site}`);
+    };
+
     return (
         <div className="card2">
             <Image src="/logo1.svg" />
@@ -25,6 +29,17 @@ function App() {
             <Text fontSize="2xl">
                 Please go to any url : shareclip.vercel.app/page
             </Text>
+            <InputGroup size="lg" mt={8} maxW="600px" w="100%" mx="auto">
+                <InputLeftAddon>shareclip.vercel.app/</InputLeftAddon>
+                <Input
+                    placeholder="mysite"
+                    value={site}
+                    onChange={(e) => setSite(e.target.value)}
+                />
+                <Button ms={5} onClick={handleGoto}>
+                    Visit
+                </Button>
+            </InputGroup>
         </div>
     );
 }
