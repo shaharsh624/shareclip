45a250884eff92b99e80b980860a6afceaf341fd
Harsh Shah <harshdev624@gmail.com>
2024-06-04 20:11:29 +0530

    Added: Feature of file sharing with text

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
     Textarea,
     Button,
@@ -9,9 +9,15 @@ import {
     Flex,
     Spacer,
     Text,
+    Input,
+    Box,
+    Link,
+    Grid,
+    GridItem,
 } from "@chakra-ui/react";
-import { Client, Databases, Query, ID } from "appwrite";
+import { Client, Databases, Query, ID, Storage } from "appwrite";
 import { useParams } from "react-router-dom";
+import { DownloadIcon } from "@chakra-ui/icons";
 
 const Page = () => {
     const { pageName } = useParams();
@@ -21,6 +27,9 @@ const Page = () => {
     const [remainingTime, setRemainingTime] = useState(null);
     const [found, setFound] = useState(false);
     const { onCopy, hasCopied } = useClipboard(text);
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+    const fileNames = useRef([]);
+    const fileIds = useRef([]);
 
     // APPWRITE
     const client = new Client()
@@ -28,28 +37,60 @@ const Page = () => {
         .setProject(import.meta.env.VITE_appwriteProjectId);
 
     const databases = new Databases(client);
+    const storage = new Storage(client);
 
-    // FIREBASE
-    // const pageCollectionRef = collection(db, "pages");
+    const uploadFilesToBucket = () => {
+        return new Promise((resolve, reject) => {
+            const uploadPromises = uploadedFiles.map(async (file) => {
+                try {
+                    const response = await storage.createFile(
+                        import.meta.env.VITE_appwriteBucketId,
+                        ID.unique(),
+                        file
+                    );
+                    console.log(response);
+                    return response.$id;
+                } catch (error) {
+                    console.log(error);
+                    throw error;
+                }
+            });
+
+            Promise.all(uploadPromises)
+                .then((fileIdsArray) => {
+                    fileIds.current = [...fileIdsArray, ...fileIds.current];
+                    resolve();
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    };
 
     const createPage = async () => {
         const creationTime = Date.now();
         const expirationTime = creationTime + validity * 1000;
 
-        // const q = query(pageCollectionRef, where("name", "==", name));
-        // const querySnapshot = await getDocs(q);
-        // if (querySnapshot.empty) {
+        try {
+            await uploadFilesToBucket();
+        } catch (error) {
+            console.error("Error uploading files:", error);
+            return;
+        }
+
         const promise = databases.createDocument(
-            "66228e3f73e9e6326fd3",
-            "66228e4a5e2573d7aab7",
+            import.meta.env.VITE_appwriteDatabaseId,
+            import.meta.env.VITE_appwriteCollectionId,
             ID.unique(),
             {
                 name: name,
                 text: text,
                 validity: validity,
                 expirationTime: expirationTime,
+                files: fileIds.current,
             }
         );
+
         promise.then(
             function (response) {
                 console.log(response);
@@ -58,87 +99,96 @@ const Page = () => {
                 console.log(error);
             }
         );
-        // } else {
-        // alert("Page already exists!");
-        // }
     };
 
     const deleteExpiredPage = async () => {
-        let promise1 = databases.listDocuments(
-            "66228e3f73e9e6326fd3",
-            "66228e4a5e2573d7aab7",
-            [Query.lessThan("expirationTime", Date.now())]
-        );
+        try {
+            const response = await databases.listDocuments(
+                import.meta.env.VITE_appwriteDatabaseId,
+                import.meta.env.VITE_appwriteCollectionId,
+                [Query.lessThan("expirationTime", Date.now())]
+            );
 
-        promise1.then(
-            function (response) {
-                response.documents.forEach((doc) => {
-                    const promise = databases.deleteDocument(
-                        "66228e3f73e9e6326fd3",
-                        "66228e4a5e2573d7aab7",
-                        doc.$id
+            for (const doc of response.documents) {
+                // Delete associated files
+                for (const fileId of doc.files) {
+                    await storage.deleteFile(
+                        import.meta.env.VITE_appwriteBucketId,
+                        fileId
                     );
-                    promise.then(
-                        function (response) {
-                            console.log(response); // Success
-                            setFound(false);
-                            setText("");
-                            setValidity(0);
-                        },
-                        function (error) {
-                            console.log(error); // Failure
-                        }
-                    );
-                    console.log(response); // Success
-                });
-            },
-            function (error) {
-                console.log(error); // Failure
+                }
+
+                // Delete document after files deletion
+                await databases.deleteDocument(
+                    import.meta.env.VITE_appwriteDatabaseId,
+                    import.meta.env.VITE_appwriteCollectionId,
+                    doc.$id
+                );
+                console.log(
+                    "Document and associated files deleted successfully."
+                );
+                setFound(false);
+                setText("");
+                setValidity(0);
             }
-        );
+        } catch (error) {
+            console.error("Error deleting expired documents:", error);
+        }
     };
 
     const getPages = async () => {
-        let promise = databases.listDocuments(
-            "66228e3f73e9e6326fd3",
-            "66228e4a5e2573d7aab7",
-            [Query.equal("name", name)]
-        );
+        try {
+            const response = await databases.listDocuments(
+                import.meta.env.VITE_appwriteDatabaseId,
+                import.meta.env.VITE_appwriteCollectionId,
+                [Query.equal("name", name)]
+            );
 
-        promise.then(
-            function (response) {
-                response.documents.forEach((doc) => {
-                    if (doc && doc.expirationTime > Date.now()) {
-                        setFound(true);
-                        setName(doc.name);
-                        setText(doc.text);
-                        const expirationTime = doc.expirationTime;
-                        const remainingTime = expirationTime - Date.now();
-                        const days = Math.floor(
-                            remainingTime / (1000 * 60 * 60 * 24)
-                        );
-                        const hours = Math.floor(
-                            (remainingTime % (1000 * 60 * 60 * 24)) /
-                                (1000 * 60 * 60)
-                        );
-                        const minutes = Math.floor(
-                            (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
-                        );
-                        const seconds = Math.floor(
-                            (remainingTime % (1000 * 60)) / 1000
-                        );
-                        setRemainingTime(
-                            `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`
-                        );
-                    }
-                    return true;
-                });
-            },
-            function (error) {
-                console.log(error);
-                return false;
-            }
-        );
+            response.documents.forEach((doc) => {
+                if (doc && doc.expirationTime > Date.now()) {
+                    setFound(true);
+                    setName(doc.name);
+                    setText(doc.text);
+                    fileIds.current = doc.files;
+                    const expirationTime = doc.expirationTime;
+                    const remainingTime = expirationTime - Date.now();
+                    const days = Math.floor(
+                        remainingTime / (1000 * 60 * 60 * 24)
+                    );
+                    const hours = Math.floor(
+                        (remainingTime % (1000 * 60 * 60 * 24)) /
+                            (1000 * 60 * 60)
+                    );
+                    const minutes = Math.floor(
+                        (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+                    );
+                    const seconds = Math.floor(
+                        (remainingTime % (1000 * 60)) / 1000
+                    );
+                    setRemainingTime(
+                        `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`
+                    );
+                }
+            });
+
+            const fileDetails = await Promise.all(
+                fileIds.current.map(async (fileId) => {
+                    const fileData = await storage.getFile(
+                        import.meta.env.VITE_appwriteBucketId,
+                        fileId
+                    );
+                    const fileUrl = storage.getFileDownload(
+                        import.meta.env.VITE_appwriteBucketId,
+                        fileId
+                    );
+                    return { name: fileData.name, url: fileUrl.href };
+                })
+            );
+
+            fileNames.current = fileDetails;
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     useEffect(() => {
@@ -156,11 +206,30 @@ const Page = () => {
         createPage();
     };
 
+    const handleFileChange = async (event) => {
+        setUploadedFiles(Array.from(event.target.files));
+        fileNames.current = Array.from(event.target.files, (file) => file.name);
+    };
+
     return (
         <Flex direction="column" p={{ base: 4, md: 8 }} className="card">
-            <Text fontSize={{ base: "3xl", md: "4xl" }}>{name}</Text>
             <FormControl>
-                <FormLabel mt={8} mb={2} fontSize={{ base: "xl", md: "2xl" }}>
+                <Flex>
+                    <Text fontSize={{ base: "3xl", md: "4xl" }}>
+                        <strong>Shareclip: {name}</strong>
+                    </Text>
+                    <Spacer />
+                    <Button
+                        colorScheme="blue"
+                        onClick={handleSubmit}
+                        isDisabled={found}
+                        size={{ base: "sm", md: "md" }}
+                        mt={{ base: "0.5rem" }}
+                    >
+                        Create
+                    </Button>
+                </Flex>
+                <FormLabel mt={4} mb={2} fontSize={{ base: "xl", md: "2xl" }}>
                     Validity
                 </FormLabel>
                 {found ? (
@@ -182,35 +251,106 @@ const Page = () => {
                         <option value={18144000}>In 1 month</option>
                     </Select>
                 )}
-                <Flex mt={8} mb={2} direction={{ base: "column", md: "row" }}>
+                <Flex mt={4} mb={2}>
                     <FormLabel fontSize={{ base: "xl", md: "2xl" }}>
-                        Enter your Text
+                        Text
                     </FormLabel>
                     <Spacer />
-                    <Button mt={{ base: 2, md: 0 }} onClick={onCopy}>
+                    <Button
+                        onClick={onCopy}
+                        isDisabled={!found}
+                        size={{ base: "sm", md: "md" }}
+                    >
                         {hasCopied ? "Copied!" : "Copy"}
                     </Button>
                 </Flex>
-                <Textarea
-                    placeholder="Write some text to share"
-                    resize="none"
-                    h={80}
-                    w="full"
-                    value={text}
-                    onChange={(event) => {
-                        setText(event.target.value);
-                    }}
-                    isReadOnly={found}
-                />
-                <Button
-                    colorScheme="blue"
-                    mt={8}
-                    mb={2}
-                    onClick={handleSubmit}
-                    isDisabled={found}
-                >
-                    Create
-                </Button>
+                <Flex gap="5" mt={0} mb={5}>
+                    <Box flex="3">
+                        <Textarea
+                            placeholder="Write some text to share"
+                            resize="none"
+                            h={80}
+                            w="full"
+                            value={text}
+                            onChange={(event) => {
+                                setText(event.target.value);
+                            }}
+                            isReadOnly={found}
+                        />
+                    </Box>
+                </Flex>
+                <Flex gap="5" mt={0} mb={2}>
+                    <FormLabel fontSize={{ base: "xl", md: "2xl" }}>
+                        Files
+                    </FormLabel>
+                    <Spacer />
+                    <Input
+                        id="fileInput"
+                        type="file"
+                        multiple
+                        onChange={handleFileChange}
+                        hidden
+                    />
+                    <Button
+                        onClick={() =>
+                            document.getElementById("fileInput").click()
+                        }
+                        isDisabled={found}
+                        size={{ base: "sm", md: "md" }}
+                    >
+                        Upload Files
+                    </Button>
+                </Flex>
+                {fileNames.current.length > 0 && found && (
+                    <Grid
+                        gap={5}
+                        templateColumns={{
+                            base: "repeat(2, 1fr)",
+                            md: "repeat(5, 1fr)",
+                        }}
+                    >
+                        {fileNames.current.map((file, index) => (
+                            <Link
+                                key={index}
+                                href={file.url}
+                                // download={file}
+                            >
+                                <GridItem
+                                    p={2}
+                                    borderWidth={1}
+                                    borderRadius={5}
+                                >
+                                    <Text>{file.name}</Text>
+                                </GridItem>
+                            </Link>
+                        ))}
+                    </Grid>
+                )}
+                {fileNames.current.length > 0 && !found && (
+                    <Grid gap={5} templateColumns="repeat(4, 1fr)">
+                        {fileNames.current.map((file, index) => (
+                            <GridItem
+                                key={index}
+                                p={2}
+                                borderWidth={1}
+                                borderRadius={5}
+                            >
+                                <Flex>
+                                    <Text>{file}</Text>
+                                    <Spacer />
+                                    <Link
+                                        href={URL.createObjectURL(
+                                            uploadedFiles[index]
+                                        )}
+                                        download={file}
+                                    >
+                                        <DownloadIcon />
+                                    </Link>
+                                </Flex>
+                            </GridItem>
+                        ))}
+                    </Grid>
+                )}
             </FormControl>
         </Flex>
     );
